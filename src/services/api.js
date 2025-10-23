50753a9be21de3ac72b5f2e647838016d045d46f
Adam Plewa <114912720+AdamPlewa@users.noreply.github.com>
2025-10-23 14:10:26 +0200

    Added chart component into coinpage

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,24 @@
-import axios from 'axios';
-
+// src/services/api.js
+import { fetchWithRetry } from './fetchWithRetry';
 
 const BASE = 'https://api.coingecko.com/api/v3';
 
-
 export async function fetchMarketData(vs_currency = 'usd', per_page = 50) {
-const url = `${BASE}/coins/markets?vs_currency=${vs_currency}&order=market_cap_desc&per_page=${per_page}&page=1&sparkline=false`;
-const res = await axios.get(url);
-return res.data;
+  const url = `${BASE}/coins/markets?vs_currency=${vs_currency}&order=market_cap_desc&per_page=${per_page}&page=1&sparkline=false`;
+  const data = await fetchWithRetry(url, { retries: 3, cacheKey: `market_${vs_currency}_${per_page}` });
+  return data;
 }
 
-
 export async function fetchCoinDetails(id) {
-const url = `${BASE}/coins/${id}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`;
-const res = await axios.get(url);
-return res.data;
+  if (!id) throw new Error('fetchCoinDetails: id is required');
+  const url = `${BASE}/coins/${id}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`;
+  const data = await fetchWithRetry(url, { retries: 3, cacheKey: `coin_${id}` });
+  return data;
+}
+
+export async function fetchCoinMarketChart(id, vs_currency = 'usd', days = 7) {
+  const url = `${BASE}/coins/${id}/market_chart?vs_currency=${vs_currency}&days=${days}&interval=daily`;
+  const data = await fetchWithRetry(url, { retries: 3, cacheKey: `chart_${id}_${vs_currency}_${days}` });
+  // data.prices ...
+  return data;
 }
