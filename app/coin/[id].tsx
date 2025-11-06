de53a8186028fbd9a0f29e80407e088058256a29
Adam Plewa <114912720+AdamPlewa@users.noreply.github.com>
2025-11-06 17:30:36 +0100

    Added SafeAreaView in case of notch/front camera coverage

--- app/coin/[id].tsx
+++ app/coin/[id].tsx
@@ -1,11 +1,22 @@
 // app/coin/[id].tsx
-import React from 'react';
-import { useLocalSearchParams } from 'expo-router';
+import { useLocalSearchParams, useNavigation } from 'expo-router';
+import React, { useEffect } from 'react';
 import Coin from '../../src/screens/Coin'; // z app/coin/[id].tsx -> ../../src/screens/Coin
 
 export default function CoinWrapper() {
   const params = useLocalSearchParams();
   const { id } = params as { id: string };
+  const navigation = useNavigation();
+
+  // ustaw bez tytułu (usuwa pokazanie "coin/[id]" w headerze/back)
+  useEffect(() => {
+    try {
+      // @ts-ignore - expo-router navigation.setOptions exists
+      navigation.setOptions({ title: '' });
+    } catch (e) {
+      // ignore
+    }
+  }, [navigation]);
 
   return <Coin route={{ params: { id } }} />;
 }
